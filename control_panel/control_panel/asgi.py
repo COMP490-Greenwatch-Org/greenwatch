fe72b46c448c837440a2cdeaccb70ef79ec3bcc7
Nick Barrios <nicholas.barrios.60@my.csun.edu>
2022-03-13 21:58:10 -0700

    Rename Django project to control_panel. Rename tensorflow_nn to neural_network.

--- control_panel/control_panel/asgi.py
+++ control_panel/control_panel/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for django_website project.
+ASGI config for control_panel project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_website.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'control_panel.settings')
 
 application = get_asgi_application()
