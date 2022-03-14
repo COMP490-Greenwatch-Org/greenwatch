9b51ba6fd6b04a547ebb3ea0bbea715d9249e81b
Nick Barrios <nicholas.barrios.60@my.csun.edu>
2022-03-14 00:38:18 -0700

    Follow example wsgi.py config.

--- control_panel/control_panel/wsgi.py
+++ control_panel/control_panel/wsgi.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 
 import os
 
-from configurations.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'control_panel.settings')
 os.environ.setdefault('DJANGO_CONFIGURATION', 'Production')
+
+from configurations.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
