b349ce93f873532fe83e02d5554811e9e7c41267
carlosg <carlos.gutierrez.873@gmail.com>
2022-03-28 13:15:19 -0700

    One to Many realtionship set up

--- control_panel/camera/models.py
+++ control_panel/camera/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
+class Camera(models.Model):
+    name = models.CharField(max_length = 10)
+
+    def __str__(self):
+        return self.name
+
 class Image(models.Model):
-    text = models.CharField(max_length=100)
+    camera = models.ForeignKey(Camera, on_delete = models.CASCADE, null = True)
+    name = models.CharField(max_length = 10)
     image = models.ImageField(null=True)
+    results = models.CharField(max_length = 100, null = True)
     date = models.DateTimeField(null=True)
 
     def __str__(self):
-        return self.text
+        return self.name
