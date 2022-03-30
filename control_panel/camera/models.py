17fdcdb6d9a7b7de23518da2bd78e887e3809049
Nick Barrios <nicholas.barrios.60@my.csun.edu>
2022-03-29 20:31:27 -0700

    Add foreign key relation to Camera and add rtsp url field

--- control_panel/camera/models.py
+++ control_panel/camera/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Camera(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE, null = True)
     name = models.CharField(max_length = 10)
+    rtsp_url = models.URLField(default = '')
 
     def __str__(self):
         return self.name
