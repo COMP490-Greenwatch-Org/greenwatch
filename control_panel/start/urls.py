6c2cfb6fa0a7803222a7337856e83ef96a78175b
Nick Barrios <nicholas.barrios.60@my.csun.edu>
2022-04-13 13:16:37 -0700

    Revert "Finished the archive page"
    This reverts commit 61274adb09eeb03849f97ba6f821d3e525e37f5a.

---
+++ control_panel/start/urls.py
@@ -0,0 +1,13 @@
+from xml.etree.ElementInclude import include
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.index, name='home'),
+    path('settings/',views.settings, name='settings'),
+    path('archive/',views.archive, name='archive'),
+    path('profile/',views.profile, name='profile'),
+    path('register/',views.register, name='register'),
+    path('about/',views.about, name='about'),
+    path('contact/',views.contact, name='contact'),
+]
---
+++ control_panel/start/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+from .forms import ExtendedUserCreationForm, NotificationsForm
+from camera.models import Camera, Image
+from .notifications import notify
+
+def index(request):
+    username = request.user.username
+    #cameras = Camera.objects.filter(user=request.user)
+    context = {'username' : username}
+    return render(request, 'start/index.html', context)
+
+@login_required
+def settings(request):
+    if request.method =='POST':
+        form = NotificationsForm(request.POST, instance=request.user.extendeduser)
+        if form.is_valid():
+            form.save()
+            return redirect('settings')
+    else:
+        form = NotificationsForm(instance=request.user.extendeduser)
+    context = {"form" : form}
+    return render(request, 'start/settings.html', context)
+
+@login_required
+def archive(request):
+    the_image = Image.objects.get(pk=1)
+    context = {'the_image' : the_image}
+    return render(request, 'start/archive.html', context)
+
+@login_required
+def profile(request):
+    return render(request, 'start/profile.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = ExtendedUserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+
+            return redirect('home')
+    else:
+        form = ExtendedUserCreationForm()
+
+    context = {'form' : form}
+    return render(request, 'start/register.html', context)
+
+def about(request):
+    return render(request, 'start/about.html')
+
+def contact(request):
+    return render(request, 'start/contact.html')
