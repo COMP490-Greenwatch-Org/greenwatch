550914f4ea9cb8d47570fd76e74dd39eb9b3c56f
Nick Barrios <nicholas.barrios.60@my.csun.edu>
2022-04-15 20:57:04 -0700

    Fix static image paths in templates.

--- control_panel/control_panel/settings.py
+++ control_panel/control_panel/settings.py
@@ -79,21 +79,10 @@ class Base(Configuration):
     # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
     #Dev
-        #DATABASES = {
-        #'default': {
-            #'ENGINE': 'django.db.backends.sqlite3',
-            #'NAME': BASE_DIR / 'db.sqlite3',
-        #}
-    #}
-    
     DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'greenwatchDB',
-        'USER': 'postgres',
-        'PASSWORD': 'lol',
-        'HOST': 'localhost',
-        'PORT': '',
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
 
