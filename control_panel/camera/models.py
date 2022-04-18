4aedfe36b448bfd278e6e650c6ea62be8119fc4b
carlosg <carlos.gutierrez.873@gmail.com>
2022-04-18 00:48:39 -0700

    Added camera form to profile page

--- control_panel/camera/models.py
+++ control_panel/camera/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+camtypes =(
+    ('wyze','WYZE'),
+    ('sonoff', 'SONOFF'),
+)
+
 class Camera(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE, null = True)
     name = models.CharField(max_length = 50)
     rtsp_url = models.URLField(default = '')
+    camera = models.CharField(max_length=10, choices=camtypes, default='wyze')
 
     def __str__(self):
         return self.name
