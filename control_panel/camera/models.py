6312f142e10f6c7ce16096ac01ba8a33d1ada3ff
Steven Condor <stcondor12@gmail.com>
2022-05-06 01:10:04 -0700

    created tasks for cron implementation

--- control_panel/camera/models.py
+++ control_panel/camera/models.py
@@ -19,8 +19,9 @@ class Image(models.Model):
     camera = models.ForeignKey(Camera, on_delete = models.CASCADE)
     name = models.CharField(max_length = 50)
     image = models.ImageField(null=True)
-    results = models.CharField(max_length = 100, null = True)
+    results = models.CharField(max_length = 100, null = True, blank = True)
     date = models.DateTimeField(null=True)
+    notified = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
